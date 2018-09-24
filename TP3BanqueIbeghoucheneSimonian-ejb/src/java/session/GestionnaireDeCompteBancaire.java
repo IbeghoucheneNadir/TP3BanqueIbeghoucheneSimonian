d8164fd02f74c40b07a266671e0c701ee1e8d4e8
Nadir <Nadir>
2018-09-24 11:13:30 +0200

    Ajout d'une méthode pour la création de compte

---
+++ TP3BanqueIbeghoucheneSimonian-ejb/src/java/session/GestionnaireDeCompteBancaire.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package session;
+
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.ejb.LocalBean;
+import tp3.CompteBancaire;
+
+/**
+ *
+ * @author MMC
+ */
+@Stateless
+@LocalBean
+public class GestionnaireDeCompteBancaire {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+    
+    public void creerComptes(CompteBancaire cb) {  
+    
+}
+    //public List<CompteBancaire> getAllComptes() {
+    
+    //}
+}
