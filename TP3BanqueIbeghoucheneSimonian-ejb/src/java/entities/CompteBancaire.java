29fcb15708588d7c201f33c94f6e37bd58c65006
IBEGHOUCHENE <ibeghouchene_nadir@yahoo.fr>
2018-10-17 21:38:20 +0200

    organisation et migration de neatbeans de 8.2 a la version 9

--- TP3BanqueIbeghoucheneSimonian-ejb/src/java/entities/CompteBancaire.java
+++ TP3BanqueIbeghoucheneSimonian-ejb/src/java/entities/CompteBancaire.java
@@ -1,93 +1,214 @@
 /*
+
  * To change this license header, choose License Headers in Project Properties.
+
  * To change this template file, choose Tools | Templates
+
  * and open the template in the editor.
+
  */
+
 package entities;
 
+
+
 import java.io.Serializable;
+
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
+
 import javax.persistence.Id;
 
+import javax.persistence.NamedQueries;
+
+import javax.persistence.NamedQuery;
+
+import javax.persistence.Table;
+
+
+
+
 /**
+
  *
- * @author MMC
+
+ * @author katzenmaul
+
  */
+
 @Entity
+
+@Table(name = "ComptesBanque", schema = "APP")
+
+@NamedQueries({
+
+    @NamedQuery(name = "Customer.findAll", query = "SELECT c FROM CompteBancaire c")})
+
 public class CompteBancaire implements Serializable {
 
+
+
     private static final long serialVersionUID = 1L;
+
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+
     private Long id;
+
     private String nom;
 
-    public CompteBancaire() {
-    }
-
-    public String getNom() {
-        return nom;
-    }
     private int solde;
 
-    public int getSolde() {
-        return solde;
+    
+
+    public CompteBancaire(){
+
     }
+
+    
+
+    public CompteBancaire(Long cbid) {
+
+        this.id = cbid;
+
+    }
+
+    
+
+    public CompteBancaire(String nom, int solde) {
+
+        this.nom = nom;
+
+        this.solde = solde;
+
+    }
+
+
+
+    public CompteBancaire(String nom, int solde, Long id) {
+
+        this(nom,solde);
+
+        this.id = id;
+
+    }
+
+
+
+    public String getNom() {
+
+        return nom;
+
+    }
+
+
+
+    public void setNom(String nom) {
+
+        this.nom = nom;
+
+    }
+
+
+
+    public int getSolde() {
+
+        return solde;
+
+    }
+
+
 
     public void setSolde(int solde) {
+
         this.solde = solde;
+
     }
 
-    public CompteBancaire(String nom, int solde) {  
-        this.nom = nom;  
-        this.solde = solde;  
-      } 
 
-    public void deposer(int montant) {  
-        solde += montant;  
-    }  
-    
-    public int retirer(int montant) {  
-        if (montant < solde) {  
-          solde -= montant;  
-          return montant;  
-        } else {  
-          return 0;  
-        }  
-      } 
+
     public Long getId() {
+
         return id;
+
     }
+
+
 
     public void setId(Long id) {
+
         this.id = id;
+
     }
 
-    @Override
-    public int hashCode() {
-        int hash = 0;
-        hash += (id != null ? id.hashCode() : 0);
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
-        if (!(object instanceof CompteBancaire)) {
-            return false;
-        }
-        CompteBancaire other = (CompteBancaire) object;
-        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
-            return false;
-        }
-        return true;
-    }
-
-    @Override
-    public String toString() {
-        return "tp2.CompteBanquaire[ id=" + id + " ]";
-    }
     
+
+  public void deposer(int montant) {  
+
+    solde += montant;  
+
+  }  
+
+    
+
+  public int retirer(int montant) {  
+
+    if (montant < solde) {  
+
+      solde -= montant;  
+
+      return montant;  
+
+    } else {  
+
+      return 0;  
+
+    }  
+
+  }  
+
+
+
+    @Override
+
+    public int hashCode() {
+
+        int hash = 0;
+
+        hash += (id != null ? id.hashCode() : 0);
+
+        return hash;
+
+    }
+
+
+
+    @Override
+
+    public boolean equals(Object object) {
+
+        // TODO: Warning - this method won't work in the case the id fields are not set
+
+        if (!(object instanceof CompteBancaire)) {
+
+            return false;
+
+        }
+
+        CompteBancaire other = (CompteBancaire) object;
+
+        return !((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id)));
+
+    }
+
+
+
+    @Override
+
+    public String toString() {
+
+        return "entities.Compte[ id=" + id + " ]";
+
+    }
+
+    
+
 }
